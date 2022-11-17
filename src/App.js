51917ff5292d22751ea632147f0b35cbfa80572d
maxiggle <clsyfriday@gmail.com>
2022-11-17 09:47:09 +0100

    updated state

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
-
-import './App.css';
-import { useState } from 'react';
-import TodoView from './Todo_view.js';
+import "./App.css";
+import { useState } from "react";
+import TodoView from "./Todo_view.js";
 import { createContext } from "react";
-
 
 export const useTodoContext = createContext({});
 
-
 const App = () => {
-    const [tasks, setTasks] = useState([]);
-    const addTask = (task) => {
-      const newTasks = [...tasks, { title: task, completed: false }];
-      setTasks(newTasks);
-    }
-    const deleteTask = (task) =>{
-     const newTasks = tasks.filter(i => i.title !== task);
-     setTasks(newTasks);
-    }
+  const [tasks, setTasks] = useState([]);
+  const addTask = (task) => {
+    const newTasks = [...tasks, { title: task, completed: false }];
+    setTasks(newTasks);
+  };
+  const deleteTask = (task) => {
+    const newTasks = tasks.filter((i) => i.title !== task);
+    setTasks(newTasks);
+  };
+
+  const updateTask = (idx, updates = {}) => {
+    const newTasks = [...tasks];
+    newTasks[idx] = updates;
+    setTasks(newTasks);
+  };
+
+  const checkTask = (idx) => {
+    updateTask(idx, { ...tasks[idx], completed: !tasks[idx].completed });
+  };
+
+  const showRemainingTask = () => {
+    return tasks.filter((e) => !e.completed).length;
+  };
   return (
     <div className="todoapp stack-large">
-     <useTodoContext.Provider value={{ tasks, addTask, deleteTask}}>
-      <TodoView/>
-     </useTodoContext.Provider>
+      <useTodoContext.Provider
+        value={{ tasks, addTask, deleteTask, showRemainingTask, checkTask }}
+      >
+        <TodoView />
+      </useTodoContext.Provider>
     </div>
   );
-}
+};
 export default App;
-
